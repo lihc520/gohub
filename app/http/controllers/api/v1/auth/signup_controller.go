489c4b09e299c4f29dd4972f4e34cf53dd9878af
eddie <eddie@wizmacau.com>
2023-06-29 12:06:13 +0800

    验证请求

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -3,10 +3,12 @@ package auth
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	v1 "github.com/lihc520/gohub/app/http/controllers/api/v1"
 	"github.com/lihc520/gohub/app/models/user"
+	"github.com/lihc520/gohub/app/requests"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 // SignupController 注册控制器
@@ -17,11 +19,8 @@ type SignupController struct {
 // IsPhoneExist 检测手机号是否被注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 
-	// 请求对象
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
-	request := PhoneExistRequest{}
+	// 初始化请求对象
+	request := requests.SignupPhoneExistRequest{}
 
 	// 解析 JSON 请求
 	if err := c.ShouldBindJSON(&request); err != nil {
@@ -35,7 +34,18 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 		return
 	}
 
-	// 检查数据库并返回响应
+	// 表单验证
+	errs := requests.ValidateSignupPhoneExist(&request, c)
+	// errs 返回长度等于零即通过，大于 0 即有错误发生
+	if len(errs) > 0 {
+		// 验证失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
+		return
+	}
+
+	//  检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
 	})
