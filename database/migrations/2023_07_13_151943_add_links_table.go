a40d07019067963c0d3aabf5c6547783ce548100
eddie <eddie@wizmacau.com>
2023-07-13 15:22:28 +0800

    友情链接模型和迁移

---
+++ database/migrations/2023_07_13_151943_add_links_table.go
@@ -0,0 +1,29 @@
+package migrations
+
+import (
+	"database/sql"
+	"github.com/lihc520/gohub/app/models"
+	"github.com/lihc520/gohub/pkg/migrate"
+	"gorm.io/gorm"
+)
+
+func init() {
+	type Link struct {
+		models.BaseModel
+
+		Name string `gorm:"type:varchar(255);not null"`
+		URL  string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Link{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Link{})
+	}
+
+	migrate.Add("2023_07_13_151943_add_links_table", up, down)
+}
